33a7edf8f83626b59e109acfa5278998e6df9d46
Anderson Moroni <andersonmoroni92@gmail.com>
2024-10-17 07:42:15 -0300

    feat: update vite

--- vite.config.js
+++ vite.config.js
@@ -16,10 +16,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        listing: resolve(__dirname, "src/product-listing/index.html"),
+        cart: resolve(__dirname, "src/playlist/index.html")
       },
     },
   },
